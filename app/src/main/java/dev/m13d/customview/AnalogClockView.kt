1a0bc166cf79a1d6a7abe4e836828d06a23565ae
Michael <3yebmb@gmail.com>
2022-02-24 19:25:05 +0400

    add: drawing outer circle

--- app/src/main/java/dev/m13d/customview/AnalogClockView.kt
+++ app/src/main/java/dev/m13d/customview/AnalogClockView.kt
@@ -18,7 +18,10 @@ class AnalogClockView @JvmOverloads constructor(
     private lateinit var mBlackPaint2: Paint
 
     init {
+        initPaints()
+    }
 
+    private fun initPaints() {
         mBlackPaint = Paint()
         with(mBlackPaint) {
             color = Color.BLACK
@@ -26,14 +29,12 @@ class AnalogClockView @JvmOverloads constructor(
             isAntiAlias = true
             style = Paint.Style.STROKE
         }
-
         mBlackPaint2 = Paint()
         with(mBlackPaint2) {
             color = Color.BLACK
             isAntiAlias = true
             style = Paint.Style.FILL
         }
-
         mRedPaint = Paint()
         with(mRedPaint) {
             color = Color.RED
@@ -44,6 +45,18 @@ class AnalogClockView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
+        drawOuterCircle(canvas)
+    }
+
+    private fun drawOuterCircle(canvas: Canvas?) {
+        mBlackPaint.strokeWidth = 5f
+        canvas?.drawCircle(
+            (measuredWidth / 2).toFloat(),
+            (measuredHeight / 2).toFloat(),
+            ((measuredWidth / 2) - 5).toFloat(),
+            mBlackPaint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
