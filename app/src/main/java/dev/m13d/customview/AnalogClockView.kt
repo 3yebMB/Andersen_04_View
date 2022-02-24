6c38560ec9905348bdf3c68c99f2065c446353f6
Michael <3yebmb@gmail.com>
2022-02-24 19:50:31 +0400

    add: getting current time & show pointer on dial

--- app/src/main/java/dev/m13d/customview/AnalogClockView.kt
+++ app/src/main/java/dev/m13d/customview/AnalogClockView.kt
@@ -6,6 +6,9 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import java.util.*
+import kotlin.math.cos
+import kotlin.math.sin
 
 class AnalogClockView @JvmOverloads constructor(
     context: Context,
@@ -16,6 +19,9 @@ class AnalogClockView @JvmOverloads constructor(
     private lateinit var mBlackPaint: Paint
     private lateinit var mRedPaint: Paint
     private lateinit var mBlackPaint2: Paint
+    private var hour: Int = 0
+    private var minute: Int = 0
+    private var second: Int = 0
 
     init {
         initPaints()
@@ -48,6 +54,8 @@ class AnalogClockView @JvmOverloads constructor(
 
         drawOuterCircle(canvas)
         drawScale(canvas)
+        getCurrentTime()
+        drawHand(canvas)
     }
 
     private fun drawOuterCircle(canvas: Canvas?) {
@@ -88,6 +96,51 @@ class AnalogClockView @JvmOverloads constructor(
         canvas?.restore()
     }
 
+    private fun getCurrentTime() {
+        val calendar = Calendar.getInstance()
+        hour = calendar.get(Calendar.HOUR)
+        minute = calendar.get(Calendar.MINUTE)
+        second = calendar.get(Calendar.SECOND)
+    }
+
+    private fun drawHand(canvas: Canvas?) {
+        drawSecond(canvas, mRedPaint)
+        mBlackPaint.strokeWidth = 10f
+        drawMinute(canvas, mBlackPaint)
+        mBlackPaint.strokeWidth = 15f
+        drawHour(canvas, mBlackPaint)
+    }
+
+    private fun drawSecond(canvas: Canvas?, paint: Paint?) {
+        val longR = measuredWidth / 2 - 60
+        val shortR = 60
+        val startX = (measuredWidth / 2 - shortR * sin(second.times(Math.PI / SECOND_DIVIDER))).toFloat()
+        val startY = (measuredWidth / 2 + shortR * cos(second.times(Math.PI / SECOND_DIVIDER))).toFloat()
+        val endX = (measuredWidth / 2 + longR * sin(second.times(Math.PI / SECOND_DIVIDER))).toFloat()
+        val endY = (measuredWidth / 2 - longR * cos(second.times(Math.PI / SECOND_DIVIDER))).toFloat()
+        paint?.let { canvas?.drawLine(startX, startY, endX, endY, it) }
+    }
+
+    private fun drawMinute(canvas: Canvas?, paint: Paint?) {
+        val longR = measuredWidth / 2 - 90
+        val shortR = 50
+        val startX = (measuredWidth / 2 - shortR * sin((minute*60+second).times(Math.PI / MINUTE_DIVIDER))).toFloat()
+        val startY = (measuredWidth / 2 + shortR * cos((minute*60+second).times(Math.PI / MINUTE_DIVIDER))).toFloat()
+        val endX = (measuredWidth / 2 + longR * sin((minute*60+second).times(Math.PI / MINUTE_DIVIDER))).toFloat()
+        val endY = (measuredWidth / 2 - longR * cos((minute*60+second).times(Math.PI / MINUTE_DIVIDER))).toFloat()
+        paint?.let { canvas?.drawLine(startX, startY, endX, endY, it) }
+    }
+
+    private fun drawHour(canvas: Canvas?, paint: Paint?) {
+        val longR = measuredWidth / 2 - 120
+        val shortR = 40
+        val startX = (measuredWidth / 2 - shortR * sin((hour*3600+minute*60+second).times(Math.PI / HOUR_DIVIDER))).toFloat()
+        val startY = (measuredWidth / 2 + shortR * cos((hour*3600+minute*60+second).times(Math.PI / HOUR_DIVIDER))).toFloat()
+        val endX = (measuredWidth / 2 + longR * sin((hour*3600+minute*60+second).times(Math.PI / HOUR_DIVIDER))).toFloat()
+        val endY = (measuredWidth / 2 - longR * cos((hour*3600+minute*60+second).times(Math.PI / HOUR_DIVIDER))).toFloat()
+        paint?.let { canvas?.drawLine(startX, startY, endX, endY, it) }
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
@@ -106,6 +159,9 @@ class AnalogClockView @JvmOverloads constructor(
     }
 
     companion object {
-        private const val DEFAULT_WIDTH = 200 
+        private const val DEFAULT_WIDTH = 200
+        private const val SECOND_DIVIDER = 30
+        private const val MINUTE_DIVIDER = 1800
+        private const val HOUR_DIVIDER = 21600
     }
 }
